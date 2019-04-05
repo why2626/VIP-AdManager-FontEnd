3f661c945d36c144b8efa5f5d8a1c6675269f515
why2626 <y602205977@gmail.com>
2019-04-05 22:54:03 +0800

    step3-showSelected

--- src/app/NewAds/step3/step3.component.ts
+++ src/app/NewAds/step3/step3.component.ts
@@ -1,20 +1,8 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { Target } from 'src/app/models';
 import { TargetService } from 'src/app/service/target.service';
-import { Router } from '@angular/router';
-import { SelectionModel } from '@angular/cdk/collections';
-import { PeriodicElement } from '../ad-position-list/ad-position-list.component';
-import { MatTableDataSource } from '@angular/material';
 
-export interface PeriodicElement {
-  targetName: string;
-  peopleEstimate: number;
-}
-/*
-const ELEMENT_DATA: PeriodicElement[] = [
-  { targetName: '', peopleEstimate: ''}
-]
-*/
 @Component({
   selector: 'app-step3',
   templateUrl: './step3.component.html',
@@ -24,41 +12,47 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class Step3Component implements OnInit {
 
   displayedColumns: string[] = ['select', 'targetName', 'peopleEstimate'];
-  data: Target[] = [];
-  isLoadingResults = true;
-  selection = new SelectionModel<PeriodicElement>(true, []);
-  public searchText : string;
+  selectedTargetColumns: string[] = ['targetName', 'peopleEstimate'];
 
-  constructor(private targetService: TargetService,private router: Router) { }
-  //dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  data: Target[] = []
+  selectedTarget: Target[] = []
+  isLoadingResults = true;
+  selection = new SelectionModel<Target>(true, [])
+  selectedLength = 0
+
+  constructor(private targetService: TargetService) { }
 
   ngOnInit() {  //get list of targets immediately.
     this.targetService.getTargets()
     .subscribe(res => {
       this.data = res;
-      // ELEMENT_DATA.push(new MatTableDataSource<ReleaseNoteData>(res as {targetName:string, peopleEstimate:number}))
       this.isLoadingResults = false;
     }, err => {
       console.log(err);
       this.isLoadingResults = false;
     });
   }
-/** Whether the number of selected elements matches the total number of rows. */
+  /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.data.length;
     return numSelected === numRows;
   }
-
   /** Selects all rows if they are not all selected; otherwise clear selection. */
- /*  masterToggle() {
+   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
         this.data.forEach(row => this.selection.select(row));
   }
- applyFilter(filterValue: string) {
-    this.filter = filterValue.trim().toLowerCase();
-  }
-*/
+  showSelectedTarget(){
+    this.selectedTarget = [] //先清空
+    this.data.forEach(
+      function(row){
+      if(this.selection.isSelected(row)){
+        this.selectedTarget.push(row)
+    }},this)
 
+    this.selectedLength = 0 //先清空
+    this.selectedLength = this.selection.selected.length
+  }
 }
